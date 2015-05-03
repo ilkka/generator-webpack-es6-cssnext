ada175ae04bf68b3f7c1b15e57c9a3a2316a13c5
Ilkka Laukkanen <ilkka@ilkka.io>
2015-05-03 11:00:17 +0300

    Add .jscsrc

--- app/index.js
+++ app/index.js
@@ -48,6 +48,10 @@ module.exports = yeoman.generators.Base.extend({
         this.templatePath('jshintrc'),
         this.destinationPath('.jshintrc')
       );
+      this.fs.copy(
+        this.templatePath('jscsrc'),
+        this.destinationPath('.jscsrc')
+      );
     }
   },
 
